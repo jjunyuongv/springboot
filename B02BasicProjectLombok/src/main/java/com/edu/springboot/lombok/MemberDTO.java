eb48fba00f274901cf0bd9bf66cde6fc25efa6ed
jjunyuongv <jjunyuongv@gmail.com>
2025-06-11 16:00:21 +0900

    도망금지

---
+++ B02BasicProjectLombok/src/main/java/com/edu/springboot/lombok/MemberDTO.java
@@ -0,0 +1,14 @@
+package com.edu.springboot.lombok;
+
+import lombok.Data;
+
+@Data
+public class MemberDTO
+{
+
+	private String id;
+	private String pass;
+	private String name;
+	private String regidate;
+	
+}
