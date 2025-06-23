e11d165668d61cbdc8b180ac0ede1b4c79957378
jjunyuongv <jjunyuongv@gmail.com>
2025-06-23 09:54:49 +0900

    커피커피커피

--- B18GeoLocations/src/main/java/com/edu/springboot/MainController.java
+++ B18GeoLocations/src/main/java/com/edu/springboot/MainController.java
@@ -1,5 +1,7 @@
 package com.edu.springboot;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.edu.springboot.mybatis.ISearchRadius;
+import com.edu.springboot.mybatis.MyFacilityDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController 
@@ -46,5 +51,39 @@ public class MainController
 		model.addAttribute("apiKey", apiKey);
 		return "03MyLocation";
 	}
+	
+	@GetMapping("/04SearchRadius.do")
+	public String geoFunc4(Model model, HttpServletRequest req) {
+		
+		model.addAttribute("apiKey", apiKey);
+		int distance = (req.getParameter("distance")==null) ?
+				0 : Integer.parseInt(req.getParameter("distance"));
+		double latTxt = (req.getParameter("latTxt")==null) ?
+				0 : Double.parseDouble(req.getParameter("latTxt"));
+		double lngTxt = (req.getParameter("lngTxt")==null) ?
+				0 : Double.parseDouble(req.getParameter("lngTxt"));
+		
+		int numberPerPage = 200;
+		int resultCount = dao.searchCount(distance, latTxt, lngTxt);
+		model.addAttribute("resultCount",
+				" / 검색결과:"+resultCount+"건");
+		model.addAttribute("selectNum",
+				Math.ceil(resultCount/numberPerPage));
+		int pageNum = (req.getParameter("pageNum")==null) ?
+				1 : Integer.parseInt(req.getParameter("pageNum"));
+		int start = ((pageNum -1) * numberPerPage) + 1;
+		int end = pageNum * numberPerPage;
+		System.out.println(distance +" "+ latTxt +" "+ lngTxt
+				+" "+ start +" "+ end);
+		ArrayList<MyFacilityDTO> searchLists = null;
+		if(distance!=0) {
+			searchLists = 
+					dao.searchRadius(distance, latTxt, lngTxt, start, end);
+		}
+		model.addAttribute("searchLists", searchLists);
+		
+		return "04SearchRadius";
+	}
+	
 }
  
