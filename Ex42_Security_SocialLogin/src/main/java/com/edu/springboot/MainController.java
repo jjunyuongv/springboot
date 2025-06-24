56c76bf034ec7085f4fbebbfe3da2b279c5eeabb
jjunyuongv <jjunyuongv@gmail.com>
2025-06-24 17:55:57 +0900

    6 / 24 Spring Data JPA02 까지

---
+++ Ex42_Security_SocialLogin/src/main/java/com/edu/springboot/MainController.java
@@ -0,0 +1,45 @@
+package com.edu.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController 
+{
+   @GetMapping("/")
+    public String root() throws Exception{
+        return "/LoginForm";
+    }
+
+   @GetMapping("/guest/welcome")
+   public String welcome1() {
+
+      return "guest/welcome1";
+   }
+   
+   @GetMapping("/member/welcome")
+   public String welcome2() {
+
+      return "member/welcome2";
+   }
+   
+   @GetMapping("/admin/welcome")
+   public String welcome3() {
+      
+      return "admin/welcome3";
+   }
+   
+   @GetMapping("/loginForm")
+   public String loginForm() {
+      
+      return "security/loginForm";
+   }
+   
+   @GetMapping("/loginError")
+   public String loginError() {
+      
+      return "security/loginError";
+   }
+}
+ 
